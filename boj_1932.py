e8d592824353bfb5e48fa80392d1de082dd3eb5e
kung0615 <kung0615@naver.com>
2022-01-23 14:40:10 +0900

    BOJ_sicknight_1783

--- boj_1932.py
+++ boj_1932.py
@@ -8,12 +8,13 @@ import sys
 input = sys.stdin.readline
 n=int(input())
 l=list()
-for i in range(n):#회의 시간들 입력
+for i in range(n):#회의 시간들 입력 /// time = sorted([tuple(map(int, input().split())) for _ in range(n)], key=lambda x:(x[1], x[0]))
     s, f = map(int,input().split())
     l.append([s,f])
-l.sort(key=lambda x:x[0])
+l.sort(key=lambda x : x[0])# 첫 시작 시간 기준으로 정렬
 l.sort(key = lambda x : x[1])#첫끝나는 시간 기준으로 정렬
-
+# 만약 2,2 1,2가 나오면 끝나느 시간으로만 정렬 하면 처음 2,2만 계산후 1,2는 계산하지 못하는 경우가 생긴다 
+# 따라서 먼저 시작시간 순으로 정렬휴 끝나는 시간을 정렬하면 1,2 2,2로 되므로 놓치지않고 계산한다.
 finish = l[0][1]
 cnt = 1#첫 시작회의 저장
 for i in range(1,n):
